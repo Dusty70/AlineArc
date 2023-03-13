4fbdc26498a5ad91a9126eb9ffbaa6468fc3b815
Dusty70 <125193185+Dusty70@users.noreply.github.com>
2023-03-13 16:00:10 +0000

    added new menu doesn't change scene

---
+++ Alien Arch - Unity/Assets/Scripts/sceneChange.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneChange : MonoBehaviour
+{
+    public void ChangeToScene(string targetSceneName)
+    {
+
+    }
+    
+}
