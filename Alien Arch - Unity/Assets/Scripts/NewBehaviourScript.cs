8d6e56738f72000f545940d6c2aa55b7f7810252
Dusty70 <125193185+Dusty70@users.noreply.github.com>
2023-03-27 15:24:14 +0100

    movment added and learnt the meaning of suffering

--- Alien Arch - Unity/Assets/Scripts/NewBehaviourScript.cs
+++ Alien Arch - Unity/Assets/Scripts/NewBehaviourScript.cs
@@ -8,12 +8,28 @@ public class NewBehaviourScript : MonoBehaviour
     //reference to a ridgedbody2Dcomponment
     private Rigidbody2D shermen = null;
 
-
-
-    public void moveLeft()
+    public void moveleft()
     {
         Vector2 newVelcity = shermen.velocity;
-        newVelcity.x = 1;
+        newVelcity.x = -Speed;
+        shermen.velocity = newVelcity;
+    }
+
+    public float jumpSpeed = 10;
+    public float Speed = 5;
+
+    public void jump()
+    {
+        Vector2 newVelcity = shermen.velocity;
+        newVelcity.y = jumpSpeed;
+        shermen.velocity = newVelcity;
+    }
+
+
+    public void moveright()
+    {
+        Vector2 newVelcity = shermen.velocity;
+        newVelcity.x = Speed;
         shermen.velocity = newVelcity;
     }
 
